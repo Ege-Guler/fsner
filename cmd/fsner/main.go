871130ff8980b04cc002d38a61fe4e6495254a50
i1nac <egeguler2003@gmail.com>
2025-06-06 12:55:30 +0300

    added cli args

--- cmd/fsner/main.go
+++ cmd/fsner/main.go
@@ -3,25 +3,73 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"regexp"
 	"sync"
 
 	"github.com/Ege-Guler/fsner/internal/scanner"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
-
 	ch := make(chan fs.FileInfo, 100)
 
-	pattern := `ders`
+	var pattern string
+	var verbose bool
+	var root string
 
-	// validate regex once
-	re := regexp.MustCompile(pattern)
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"V"},
+		Usage:   "Print the version of fsner",
+	}
 
-	wg.Add(1)
-	go scanner.SearchFile("/", re, false, ch, &wg)
+	app := &cli.App{
+		Name:    "fsner",
+		Usage:   "A file system search tool",
+		Version: "0.1.0",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "pattern",
+				Aliases:     []string{"p"},
+				Usage:       "Pattern to search for in file names",
+				Required:    true,
+				Destination: &pattern,
+			},
+			&cli.BoolFlag{
+				Name:        "verbose",
+				Aliases:     []string{"v"},
+				Usage:       "Enable verbose output, defaults to false",
+				Required:    false,
+				Value:       false,
+				Destination: &verbose,
+			},
+			&cli.StringFlag{
+				Name:        "root",
+				Aliases:     []string{"r"},
+				Usage:       "Root directory to start the search from, defaults to /",
+				Value:       "/",
+				Required:    true,
+				Destination: &root,
+			}},
+		Action: func(c *cli.Context) error {
+			if pattern == "" {
+				return fmt.Errorf("pattern is required")
+			}
+			// validate regex once
+			re := regexp.MustCompile(pattern)
+			wg.Add(1)
+			go scanner.SearchFile(root, re, verbose, ch, &wg)
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "\nError running app: %v\n", err)
+		return
+	}
 
 	go func() {
 		wg.Wait()
